1c9304486a22c42fb81af3b8738cad25cc529f2a
Ivan Bukirev <i.bukirev@gmail.com>
2023-12-30 18:00:09 +0300

    commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BinOps bins = new BinOps();
-        System.out.println(bins.sum("100","10101"));
-        System.out.println(bins.mult("10","10011"));
+        System.out.println(bins.sum("1001","10101"));
+        System.out.println(bins.mult("11","10011"));
     }
 }
